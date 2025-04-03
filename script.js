e88d2a784850806d519cd5707f23a9850d2583fd
Nate Siller <siller97@gmail.com>
2025-04-03 00:18:13 -0400

    set up

--- script.js
+++ script.js
@@ -20,11 +20,30 @@ function getComputerChoice(){
 function getHumanChoice() {
     //use prompt to get input
     let sign = prompt("Please enter rock, paper, or scissors");
-    if (sign === null) {
-        console.log("OK, maybe next time.");
+    if (sign === ""){
+        console.log("Enter only rock, paper, or scissors");
     }
-
+    else if(sign === "rock"){
+        return sign;
+    }
+    else if(sign === "paper"){
+        return sign;
+    }
+    else{
+        return sign;
+    }
 }
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice){
+    //need to pull the answers from the previous functions
+    //after pulling them, compare by string only to see who wins
+    //write it so it displays which one won ("Paper beats rock!")
+    if (humanChoice === computerChoice){
+        return "You tied!"
+    }
+    else if (){}
+}
+const humanSelection = getHumanChoice();
+const computerChoice = getComputerChoice();
 //write a function that takes both scores, compares them, and
 //then increments the winners score and logs a winner announcement
