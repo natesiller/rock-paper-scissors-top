e748b177ab03a8d00b73a7a7982d9df3f203d3b9
Nate Siller <siller97@gmail.com>
2025-04-05 00:06:22 -0400

    rewrote and used a guide

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
-function compareChoices(){
+function getHumanChoice() {
     let userInput = prompt("Enter your choice of rock, paper, or scissors");
     userInput = userInput.toLowerCase();
+    return userInput;
+}
+
+function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const rng = Math.floor(Math.random() * choices.length);
     let computerChoice = choices[rng];
+    return computerChoice;
+}
 
-    if (computerChoice === userInput){
-        alert("It's a tie");
-    }
-    else if (
-        (userInput === "rock" && computerChoice === "scissors") ||
-        (userInput === "paper" && computerChoice === "rock") ||
-        (userInput === "scissors" && computerChoice === "paper")){
-        alert("You win!");
-    }
-    else if(choices.includes(userInput)){
-        alert("Computer wins!");
-    }
-    else{
-        alert("Please enter a rock, paper, or scissors");
-    }
+function playRound(humanChoice, computerChoice){
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+}
+
+if (computerChoice === userInput){
+    console.log("It's a tie");
+}
+else if (
+    (userInput === "rock" && computerChoice === "scissors") ||
+    (userInput === "paper" && computerChoice === "rock") ||
+    (userInput === "scissors" && computerChoice === "paper")){
+    console.log("You win!");
+}
+else if(choices.includes(userInput)){
+    console.log("Computer wins!");
+}
+else{
+    console.log("Please enter a rock, paper, or scissors");
 }
 let result = compareChoices();
 console.log(result);
