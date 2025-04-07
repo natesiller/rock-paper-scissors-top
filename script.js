94fed0b0bf6e428a908a512b913deee294a91ec9
Nate Siller <siller97@gmail.com>
2025-04-06 21:22:52 -0400

    fixed and working

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
-function getHumanChoice() {
-    let userInput = prompt("Enter your choice of rock, paper, or scissors");
-    userInput = userInput.toLowerCase();
-    return userInput;
-}
+let userInput = prompt("Enter your choice of rock, paper, or scissors");
+userInput = userInput.toLowerCase();
+
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
+    let choices = ["rock", "paper", "scissors"]
     const rng = Math.floor(Math.random() * choices.length);
-    let computerChoice = choices[rng];
-    return computerChoice;
+    return choices[rng]
 }
 
 function playRound(humanChoice, computerChoice){
-    humanChoice = getHumanChoice();
+    humanChoice = userInput;
     computerChoice = getComputerChoice();
+
+    if (humanChoice === computerChoice){
+        console.log("It's a tie!")
+    }
+    else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")){
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+
+    }
+    else if((computerChoice === "rock" && humanChoice=== "scissors") ||
+        (computerChoice === "paper" && humanChoice === "rock") ||
+        (computerChoice === "scissors" && humanChoice === "paper")){
+        console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else{
+        console.log("Please enter a rock, paper, or scissors");
+    }
+}
+// let humanSelection = userInput;
+// let compuerSelection = getComputerChoice();
+
+function playGame(){
+    for (let i = 1; i < 5; i++){
+       playRound();
+       let iterationUserInput = prompt("Enter your choice of rock, paper, or scissors");
+       userInput = iterationUserInput;
+       }
+    alert(`Human score = ${humanScore} \nComputer score = ${computerScore}`);
 }
 
-if (computerChoice === userInput){
-    console.log("It's a tie");
-}
-else if (
-    (userInput === "rock" && computerChoice === "scissors") ||
-    (userInput === "paper" && computerChoice === "rock") ||
-    (userInput === "scissors" && computerChoice === "paper")){
-    console.log("You win!");
-}
-else if(choices.includes(userInput)){
-    console.log("Computer wins!");
-}
-else{
-    console.log("Please enter a rock, paper, or scissors");
-}
-let result = compareChoices();
-console.log(result);
+console.log(playGame());
